dd10fa1c18b000a27aa218bed350a9451a159ffc
Marioo295 <mramosgaliana@gmail.com>
2022-10-05 10:07:37 +0200

    REDUX: teoría redux

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import TeoriaRedux from "./components/TeoriaRedux";
+
+function App() {
+    return (
+        <div style={{textAlign: "center"}}>
+            <h1>REDUX</h1>
+            <hr></hr>
+            <TeoriaRedux></TeoriaRedux>
+        </div>
+    );
+}
+
+export default App;
